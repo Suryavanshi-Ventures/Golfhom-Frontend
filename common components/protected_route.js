3a29fcc486f408045de65c5006c30bc0d419dfec
XtremyS <rajulverma7777@gmail.com>
2023-05-04 17:18:34 +0530

    implemented protected route for all dashboard pages

--- common components/protected_route.js
+++ common components/protected_route.js
@@ -1,18 +1,30 @@
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { message } from "antd";
 
 const ProtectedRoute = ({ children }) => {
   const router = useRouter();
-
   useEffect(() => {
     // check if user is authenticated
-    const isAuthenticated = sessionStorage.getItem("token"); // your authentication logic here
+    const IsAuthenticated = sessionStorage.getItem("token"); // your authentication logic here
+    const Token = IsAuthenticated;
 
-    console.log(isAuthenticated);
-    if (!isAuthenticated) {
-      // redirect to login page
-      router.push("/");
-    }
+    //! User Get Profile API Call
+    const User = axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/v1/auth/profile`,
+      { headers: { Authorization: `Bearer ${Token}` } }
+    );
+    User.then((response) => {
+      console.log(response, "user authenticated");
+    }).catch((error) => {
+      if (!IsAuthenticated || error.response.status === 401) {
+        // redirect to login page
+        router.push("/");
+      }
+      console.log(error, "user not authenticated");
+      message.error(error.response.data.message);
+    });
   }, [router]);
 
   return <>{children}</>;
