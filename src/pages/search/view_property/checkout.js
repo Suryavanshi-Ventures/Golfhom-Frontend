3ef0f7c8fea9f3d3925b512e23fcbf6e4588a5b9
DeepBoda <deepboda18@gmail.com>
2023-05-26 22:32:12 +0530

    build -fix

--- src/pages/search/view_property/checkout.js
+++ src/pages/search/view_property/checkout.js
@@ -1,30 +1,13 @@
 import React from "react";
-import { useStripe, useElements } from "@stripe/react-stripe-js";
+import { Elements, useStripe, useElements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import { Button } from "antd";
 
 const Checkout = () => {
   const stripe = useStripe();
   const elements = useElements();
-  const stripePromise = loadStripe(
-    `${process.env.NEXT_PUBLIC_STRIPE_TEST_KEY}`
-  );
 
   const BookingHotelDone = async () => {
-    // const BookingRes = axios.post(
-    //   `${process.env.NEXT_PUBLIC_API_URL}/v1/booking`,
-    //   {
-    //     propertyId: SpecificPropAPIData.id,
-    //     from: BookingDate[0],
-    //     to: BookingDate[1],
-    //   },
-    //   {
-    //     headers: {
-    //       Authorization: `Bearer ${ContextUserDetails.UserState}`,
-    //     },
-    //   }
-    // );
-
     if (!stripe || !elements) {
       // Stripe.js hasn't yet loaded.
       // Make sure to disable form submission until Stripe.js has loaded.
@@ -32,7 +15,6 @@ const Checkout = () => {
     }
 
     const { error } = await stripe.confirmPayment({
-      //`Elements` instance that was used to create the Payment Element
       elements,
       confirmParams: {
         return_url: "http://localhost:3000/search/view_property/success",
@@ -51,4 +33,12 @@ const Checkout = () => {
   );
 };
 
-export default Checkout;
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_TEST_KEY);
+
+const WrappedCheckout = () => (
+  <Elements stripe={stripePromise}>
+    <Checkout />
+  </Elements>
+);
+
+export default WrappedCheckout;
