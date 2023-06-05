ee272417ac57b5b1e40b339b78642eb3f16df4ec
XtremyS <rajulverma7777@gmail.com>
2023-06-05 23:02:43 +0530

    fixed un removed images

--- src/pages/home.js
+++ src/pages/home.js
@@ -45,7 +45,8 @@ const Home = () => {
     const GetPropDataFunc = async () => {
       try {
         const GetPropertyDataRes = await axios.get(
-          `${process.env.NEXT_PUBLIC_API_URL
+          `${
+            process.env.NEXT_PUBLIC_API_URL
           }/v1/property?limit=6&latitude=${34.098967}&longitude=${-118.246683}`
         );
         if (GetPropertyDataRes.status === 200) {
@@ -151,17 +152,20 @@ const Home = () => {
           UrlParamsGeoData?.latitude
         )}&longitude=${encodeURIComponent(
           UrlParamsGeoData?.longitude
-        )}&location_name=${UrlParamsGeoData?.location_name
+        )}&location_name=${
+          UrlParamsGeoData?.location_name
         }&nights=${NightsCounter}&guest=${encodeURIComponent(
           adult + child
         )}&adults=${encodeURIComponent(adult)}&childs=${encodeURIComponent(
           adult
-        )}&from=${UrlParamsDateRange[0]
-          ? UrlParamsDateRange[0]
-          : moment().format("MM-DD-YYYY")
-        }&to=${UrlParamsDateRange[1]
-          ? UrlParamsDateRange[1]
-          : moment().format("MM-DD-YYYY")
+        )}&from=${
+          UrlParamsDateRange[0]
+            ? UrlParamsDateRange[0]
+            : moment().format("MM-DD-YYYY")
+        }&to=${
+          UrlParamsDateRange[1]
+            ? UrlParamsDateRange[1]
+            : moment().format("MM-DD-YYYY")
         }&limit=10`
       );
     }
@@ -612,15 +616,16 @@ const Home = () => {
             </Col>
 
             <Col xl={4} lg={4} className={HomeCss.midImage}>
-
               <h6 className={HomeCss.midImgTitle}>
                 <Link
                   href={`/search?latitude=${encodeURIComponent(
                     latitude
                   )}&longitude=${encodeURIComponent(longitude)}`}
-                  style={{ textDecoration: 'none', color: 'inherit' }}>Sao Paulo, New york </Link>
+                  style={{ textDecoration: "none", color: "inherit" }}
+                >
+                  Sao Paulo, New york{" "}
+                </Link>
               </h6>
-
             </Col>
 
             <Col xl={4} lg={4} className={HomeCss.twoImgContainer}>
@@ -630,7 +635,10 @@ const Home = () => {
                     href={`/search?latitude=${encodeURIComponent(
                       latitude
                     )}&longitude=${encodeURIComponent(longitude)}`}
-                    style={{ textDecoration: 'none', color: 'inherit' }}>The Madrid EDITION </Link>
+                    style={{ textDecoration: "none", color: "inherit" }}
+                  >
+                    The Madrid EDITION{" "}
+                  </Link>
                 </h6>
               </div>
               <div>
@@ -639,9 +647,10 @@ const Home = () => {
                     href={`/search?latitude=${encodeURIComponent(
                       latitude
                     )}&longitude=${encodeURIComponent(longitude)}`}
-                    style={{ textDecoration: 'none', color: 'inherit' }}
+                    style={{ textDecoration: "none", color: "inherit" }}
                   >
-                    The Westin Anaheim Resort{" "}</Link>
+                    The Westin Anaheim Resort{" "}
+                  </Link>
                 </h6>
               </div>
             </Col>
@@ -727,7 +736,7 @@ const Home = () => {
                             width={20}
                             height={14}
                             className={HomeCss.rightArrow}
-                            src="/images/vector/right_Arrow.svg"
+                            src="/images/vector/right_arrow.svg"
                             alt="iconImage"
                           ></Image>
                         </div>
