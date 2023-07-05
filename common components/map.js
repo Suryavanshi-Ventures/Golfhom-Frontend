65e62131884cdf74e521c9f6aee15b2ad7db71e2
Xtremy <rajulverma7777@gmail.com>
2023-07-05 13:02:10 +0530

    changed styling of search by golf course input

--- common components/map.js
+++ common components/map.js
@@ -1,6 +1,5 @@
-import { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
-import React from "react";
 
 const containerStyle = {
   width: "100%",
@@ -10,60 +9,91 @@ const containerStyle = {
 
 let center = {};
 
-const markers = [];
+let markers = [];
 
 const GoogleMaps = (PropertyData) => {
-  if (PropertyData.data?.length > 1) {
-    const LangLats = PropertyData.data.forEach((element, index) => {
-      // console.log(element.latitude, "MULTIPLE PROP LAT LANG");
-      // console.log(element.longitude, "MULTIPLE PROP LAT LANG");
-
-      markers.push({
-        lat: Number(element.latitude),
-        lng: Number(element.longitude),
-      });
-    });
-
-    center = {
-      lat: markers[0]?.lat,
-      lng: markers[0]?.lng,
-    };
-  }
-
-  // console.log(PropertyData.data[0]?.latitude, "SINGLE PROP LAT LANG");
-  // console.log(PropertyData.data[0]?.longitude, "SINGLE PROP LAT LANG");
-
-  markers.push({
-    lat: Number(PropertyData.data[0]?.latitude),
-    lng: Number(PropertyData.data[0]?.longitude),
-  });
-
-  center = {
-    lat: Number(PropertyData.data[0]?.latitude),
-    lng: Number(PropertyData.data[0]?.longitude),
-  };
-
+  const [map, setMap] = useState(null);
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
-    googleMapsApiKey: "AIzaSyBfYxWDRxvC8H61TYmlUpzzQBWyQ-QMGkU",
+    googleMapsApiKey: `${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`,
   });
 
-  const [map, setMap] = React.useState(null);
-
-  const onLoad = useCallback(function callback(map) {
-    setMap(map);
+  useEffect(() => {
+    return () => {};
   }, []);
 
-  const onUnmount = React.useCallback(function callback(map) {
+  const onLoad = useCallback(function callback(Val) {
+    setMap(Val);
+  }, []);
+
+  const onUnmount = useCallback(function callback(map) {
     setMap(null);
   }, []);
 
-  useEffect(() => {
-    return () => {
-      markers;
-      center;
+  if (PropertyData.data?.length === 1) {
+    console.log(PropertyData.data, "SINGLE LAT LONG");
+
+    if (markers.length >= 1) {
+      markers.splice(0, 1);
+    }
+    console.log(markers, "MARKERRRR SINGLE ");
+
+    markers = [
+      {
+        lat: Number(PropertyData.data[0]?.latitude),
+        lng: Number(PropertyData.data[0]?.longitude),
+      },
+    ];
+    center = {
+      lat: Number(PropertyData.data[0]?.latitude),
+      lng: Number(PropertyData.data[0]?.longitude),
     };
-  }, []);
+
+    // const LangLats = PropertyData.data.forEach((element, index) => {
+    //   markers.push({
+    //     lat: Number(element?.latitude),
+    //     lng: Number(element?.longitude),
+    //   });
+    // });
+    // // //* IF LAT LONG GOES MORE THAN 10 THAN REMOVING OLD LAT LONG FROM ARRA
+    // // if (markers.length > 10) {
+    // //   markers.splice(0, 10);
+    // // }
+    // console.log(markers, " MARKEEEEEEEEEEEEEEEEER");
+    // center = {
+    //   lat: Number(markers[0]?.lat),
+    //   lng: Number(markers[0]?.lng),
+    // };
+  } else {
+    console.log(PropertyData.data, "MULTIPLE DATA");
+
+    const LangLats = PropertyData.data.forEach((element, index) => {
+      markers.push({
+        lat: Number(element?.latitude),
+        lng: Number(element?.longitude),
+      });
+    });
+    // //* IF LAT LONG GOES MORE THAN 10 THAN REMOVING OLD LAT LONG FROM ARRA
+    if (markers.length > 10) {
+      markers.splice(0, 10);
+    }
+
+    center = {
+      lat: Number(markers[0]?.lat),
+      lng: Number(markers[0]?.lng),
+    };
+    console.log(markers, "MULTIPLE LAT LONG");
+  }
+
+  // markers.push({
+  //   lat: Number(PropertyData.data[0]?.latitude),
+  //   lng: Number(PropertyData.data[0]?.longitude),
+  // });
+
+  // center = {
+  //   lat: Number(PropertyData.data[0]?.latitude),
+  //   lng: Number(PropertyData.data[0]?.longitude),
+  // };
 
   return isLoaded ? (
     <GoogleMap
